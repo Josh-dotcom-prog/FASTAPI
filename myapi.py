fd868a2c6267a4a3594bbc5b02b108c0f6a6a1a4
Joshua Edyangu <edyangujoshua@gmail.com>
2026-07-16 21:21:13 +0300

    creted post method

--- myapi.py
+++ myapi.py
@@ -31,3 +31,14 @@ def get_student(*, student_id: int, name: Optional[str] = None, test : int):
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not found"}
+
+
+#creating a student
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student : Student):
+    if student_id in students:
+        return {"Error": "Student already exists"}
+
+    students[student_id] = student
+    return students[student_id]
+
